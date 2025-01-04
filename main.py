885348bfd93fe06a349894ab4815ff6830789790
Imperator <greatmimperator@gmail.com>
2025-01-04 06:51:39 +0300

    Добавил перевод из документа в фото

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ async def download_and_convert_video(file_info: File) -> tuple[str, str]:
     ffmpeg.input(tmp_file_path).output(mp4_file_path).run()
     return tmp_file_path, mp4_file_path
 
+async def download_photo(file_info: File) -> str:
+    unique_filename = f"{uuid.uuid4()}"
+    tmp_file_path = os.path.join(tempfile.gettempdir(), unique_filename)
+    await file_info.download_to_drive(tmp_file_path)
+    return tmp_file_path
+
 # Функция для пересылки сообщений в каналы
 async def forward_to_channels_with_check(update: Update, context: CallbackContext, admin_ids: list[int], channel_ids: list[int]):
     bot = context.bot
@@ -35,16 +41,16 @@ async def forward_to_channels_with_check(update: Update, context: CallbackContex
         for channel_id in channel_ids:
             await context.bot.send_video(chat_id=channel_id, video=message.video.file_id, caption=message.caption)
     elif message.document:
-        if message.document.mime_type == "video/quicktime":
-            file_info = await context.bot.get_file(message.document.file_id)
+        document = message.document
+        if document.mime_type == "video/quicktime":
+            file_info = await context.bot.get_file(document.file_id)
             input_file_path = None
             converted_video_path = None
             try:
                 input_file_path, converted_video_path = await download_and_convert_video(file_info)
-                if converted_video_path:
-                    with open(converted_video_path, 'rb') as video:
-                        for channel_id in channel_ids:
-                            await context.bot.send_video(chat_id=channel_id, video=video, filename=message.document.file_name)
+                with open(converted_video_path, 'rb') as video:
+                    for channel_id in channel_ids:
+                        await context.bot.send_video(chat_id=channel_id, video=video, filename=message.document.file_name)
             except ffmpeg.Error as e:
                 await bot.send_message(
                     chat_id=user_chat_id,
@@ -56,10 +62,21 @@ async def forward_to_channels_with_check(update: Update, context: CallbackContex
                     os.remove(input_file_path)
                 if converted_video_path is not None and os.path.exists(converted_video_path):
                     os.remove(converted_video_path)
+        elif message.document.mime_type.startswith("image/"):
+            file_info = await context.bot.get_file(document.file_id)
+            photo_path = None
+            try:
+                photo_path = await download_photo(file_info)
+                with open(photo_path, 'rb') as photo:
+                    for channel_id in channel_ids:
+                        await context.bot.send_photo(chat_id=channel_id, photo=photo, filename=message.document.file_name)
+            finally:
+                if photo_path is not None and os.path.exists(photo_path):
+                    os.remove(photo_path)
+            await context.bot.send_photo(chat_id=user_chat_id, photo=document.file_id)
         else:
-            file = message.document
             for channel_id in channel_ids:
-                await context.bot.send_document(chat_id=channel_id, document=file.file_id, caption=message.caption)
+                await context.bot.send_document(chat_id=channel_id, document=document.file_id, caption=message.caption)
     elif message.text:
         for channel_id in channel_ids:
             await context.bot.send_message(chat_id=channel_id, text=message.text)
